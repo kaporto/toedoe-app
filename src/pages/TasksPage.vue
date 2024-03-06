1b040c38f4a63df2cc8fa6ca390d9537ec82aef0
karine <karine.porto@gmail.com>
2024-03-06 16:42:54 +0000

    Adding new feature in our Vue js app to update data

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -13,7 +13,7 @@
 
 
                     <!-- List of  uncompleted tasks -->
-                    <Tasks :tasks="uncompletedTasks" />
+                    <Tasks :tasks="uncompletedTasks" @updated="handleUpdatedTask"/>
 
 
                     <!-- show toggle button-->
@@ -40,7 +40,7 @@
 
 <script setup>
 import { computed, onMounted, ref } from "vue";
-import { allTasks, createTask } from "../http/task-api";
+import { allTasks, createTask, updateTask } from "../http/task-api";
 import Tasks from "../components/tasks/Tasks.vue";
 import NewTask from "../components/tasks/NewTask.vue"
 
@@ -66,5 +66,13 @@ const showCompletedTasks = ref(false)
 const handleAddedTask = async (NewTask) => {
     const { data: createdTask } = await createTask(NewTask)
     tasks.value.unshift(createdTask.data)
+}
+
+const handleUpdatedTask = async (task) => {
+    const { data:updatedTask} = await updateTask(task.id, {
+        name: task.name
+    })
+    const currentTask = tasks.value.find(item => item.id == task.id)
+    currentTask.name = updatedTask.data.name
 }
 </script>
