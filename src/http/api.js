f2abbc4be26fdead39ed2e67d9800701596f18f7
karine <karine.porto@gmail.com>
2024-03-08 16:33:01 +0000

    Bulding Login form

--- src/http/api.js
+++ src/http/api.js
@@ -2,8 +2,10 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL
+    baseURL: import.meta.env.VITE_BASE_URL,
+    withXSRFToken: true // <---
 })
 
 export default api
