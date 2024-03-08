ccd54e26ac42b67d0e1cefc116d008ec34fc5cfb
karine <karine.porto@gmail.com>
2024-03-08 17:13:31 +0000

    Replacing the login button with a dropdown

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -45,10 +45,17 @@
                             <router-link :to="{ name: 'register' }" class="btn btn-danger ms-2">Register</router-link>
                         </li>
                     </template>
-                    <template v-else>
-                        <li class="nav-item">
-                            <a href="#" class="btn btn-outline-secondary ms-2" @click.prevent="logout">Logout</a>
+                    <template v-else>                        
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" :class="toggleClass" @click.prevent="toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                {{ store.user.name }}
+                            </a>
+                            <ul class="dropdown-menu" :class="toggleClass" >
+                                <li><a href="#" class="dropdown-item" @click.prevent="logout">Logout</a></li>                                
+                            </ul>
                         </li>
+
                     </template>
 
                 </ul>
@@ -60,15 +67,21 @@
 
 
 <script setup>
+import { ref, computed } from "vue";
 import { useRouter } from "vue-router";
 import { useAuthStore } from "../stores/auth";
 
 const router = useRouter()
 const store = useAuthStore()
+const isOpen = ref(false)
 
 const logout = async () => {
     await store.handleLogout()
+    isOpen.value = false
     router.push({ name: 'login' })
 }
 
+const toggle = () => isOpen.value = !isOpen.value
+
+const toggleClass = computed( () => isOpen.value == true ? 'show' : '')
 </script>
