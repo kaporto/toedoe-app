916f4e027699bdcba83f7edb4a2a984cd7b28f3b
karine <karine.porto@gmail.com>
2024-03-13 16:01:22 +0000

    Highlighting active filter item

--- src/components/summaries/filter/SummaryFilter.vue
+++ src/components/summaries/filter/SummaryFilter.vue
@@ -1,9 +1,10 @@
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { startOfWeek, endOfWeek, subWeeks,format } from "date-fns";
 import Dropdown from "../../dropdown/Dropdown.vue";
 import DropdownItem from "../../dropdown/DropdownItem.vue";
 import DropdownTrigger from "../../dropdown/DropdownTrigger.vue";
+import FilterItem from "../filter/FilterItem.vue";
 
 const filterItems = computed(() => {
     const thisWeekStart = format(startOfWeek(new Date()),'d MMM')
@@ -19,16 +20,31 @@ const filterItems = computed(() => {
         'lastmonth': 'Last month'
     }
 })
+
+const emit = defineEmits(['update'])
+
+const filter = (period) => {
+    activeFilterKey.value = period
+    emit('update',activeFilter.value)
+}
+
+onMounted(() => emit('update', activeFilter.value))
+
+const activeFilterKey = ref('thisweek')
+const activeFilter = computed(() => filterItems.value[activeFilterKey.value] || filterItems.value.thisweek)
+
 </script>
 
 <template>
     <Dropdown>
         <template v-slot:trigger="{ toggleClass, toggle }">
-            <DropdownTrigger class="btn btn-outline-secondary" :class="toggleClass" type="button" @click="toggle">Filter
+            <DropdownTrigger class="btn btn-outline-secondary" :class="toggleClass" type="button" @click="toggle">
+                {{ activeFilter }}
             </DropdownTrigger>
         </template>
         <template v-slot:menu="{ toggle }">
-            <DropdownItem v-for="(value, key) in filterItems" :key="key" href="#" @click.prevent="toggle">{{ value }}
+            <DropdownItem v-for="(value, key) in filterItems" :key="key" href="#" @click.prevent="toggle(), filter(key)">
+               <FilterItem :text="value" :selected="key == activeFilterKey"/>
             </DropdownItem>
         </template>
     </Dropdown>
--- src/pages/SummaryPage.vue
+++ src/pages/SummaryPage.vue
@@ -8,8 +8,8 @@
                 <div class="col-md-8 offset-md-2">
 
                     <div class="d-flex justify-content-between align-items-center mb-4">
-                        <h1> Summary <small class="text-muted">(Tasks this week)</small></h1>
-                        <SummaryFilter />
+                        <h1> Summary <small class="text-muted fs-4">{{  selectedFilter }}</small></h1>
+                        <SummaryFilter @update="selectedFilter = $event"/>
                     </div>                    
 
                     <div v-for="(tasks, description) in summaries" :key="description">
@@ -31,7 +31,7 @@
 <script setup>
 
 import { useSummaryStore } from "../stores/summary";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { storeToRefs } from "pinia";
 import Summaries from "../components/summaries/Summaries.vue"
 import SummaryFilter from "../components/summaries/filter/SummaryFilter.vue";
@@ -41,6 +41,8 @@ const store = useSummaryStore()
 const { summaries } = storeToRefs(store)
 
 const { fetchTasksSummary } = store
+
+const selectedFilter = ref('')
 
 onMounted(async () => {
 
